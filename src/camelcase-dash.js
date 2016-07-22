299556f90522384d55f7d318f427e5692c7dde59
Noah H. Smith <noah@batteredoats.com>
2016-07-22 08:33:12 -0700

    using es6 fat arrow syntax

--- src/camelcase-dash.js
+++ src/camelcase-dash.js
@@ -14,7 +14,7 @@ class CamelCaseDashConverter {
    * @returns {string} Converted string.
    */
   camelize(str) {
-    return str.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase(); });
+    return str.replace(/-([a-z])/g,  match => match[1].toUpperCase());
   }
 
   /**
