e6541345b67082e5722b47167802d71ce4505309
Noah H. Smith <noah@batteredoats.com>
2016-08-02 11:34:06 -0700

    ordering proxied methods alphabetically

--- src/collection-proxy.js
+++ src/collection-proxy.js
@@ -45,51 +45,54 @@ class CollectionProxy {
   get models() { return this.content.models; }
 
   at() { return this.content.at(...arguments); }
+  chain() { return this.content.chain(...arguments); }
+  contains() { return this.content.contains(...arguments); }
+  countBy() { return this.content.countBy(...arguments); }
+  difference() { return this.content.difference(...arguments); }
+  every() { return this.content.every(...arguments); }
+  filter() { return this.content.filter(...arguments); }
+  find() { return this.content.find(...arguments); }
+  findIndex() { return this.content.findIndex(...arguments); }
+  findLastIndex() { return this.content.findLastIndex(...arguments); }
   findWhere() { return this.content.findWhere(...arguments); }
+  first() { return this.content.first(...arguments); }
+  forEach() { return this.content.forEach(...arguments); }
   get() { return this.content.get(...arguments); }
+  groupBy() { return this.content.groupBy(...arguments); }
+  indexBy() { return this.content.indexBy(...arguments); }
+  indexOf() { return this.content.indexOf(...arguments); }
+  initial() { return this.content.initial(...arguments); }
+  invoke() { return this.content.invoke(...arguments); }
+  isEmpty() { return this.content.isEmpty(...arguments); }
+  last() { return this.content.last(...arguments); }
+  lastIndexOf() { return this.content.lastIndexOf(...arguments); }
   map() { return this.content.map(...arguments); }
+  max() { return this.content.max(...arguments); }
+  min() { return this.content.min(...arguments); }
+  off() { return this.content.off(...arguments); }
+  on() { return this.content.on(...arguments); }
+  once() { return this.content.once(...arguments); }
+  partition() { return this.content.partition(...arguments); }
   pluck() { return this.content.pluck(...arguments); }
   pop() { return this.content.pop(...arguments); }
   push() { return this.content.push(...arguments); }
   reduce() { return this.content.reduce(...arguments); }
+  reduceRight() { return this.content.reduceRight(...arguments); }
+  reject() { return this.content.reject(...arguments); }
+  rest() { return this.content.rest(...arguments); }
+  sample() { return this.content.sample(...arguments); }
   set() { return this.content.set(...arguments); }
   shift() { return this.content.shift(...arguments); }
+  shuffle() { return this.content.shuffle(...arguments); }
+  size() { return this.content.size(...arguments); }
   slice() { return this.content.slice(...arguments); }
+  some() { return this.content.some(...arguments); }
   sort() { return this.content.sort(...arguments); }
+  sortBy() { return this.content.sortBy(...arguments); }
+  toArray() { return this.content.toArray(...arguments); }
   toJSON() { return this.content.toJSON(...arguments); }
   where() { return this.content.where(...arguments); }
-  forEach() { return this.content.forEach(...arguments); }
-  reduceRight() { return this.content.reduceRight(...arguments); }
-  find() { return this.content.find(...arguments); }
-  findIndex() { return this.content.findIndex(...arguments); }
-  findLastIndex() { return this.content.findLastIndex(...arguments); }
-  filter() { return this.content.filter(...arguments); }
-  reject() { return this.content.reject(...arguments); }
-  every() { return this.content.every(...arguments); }
-  some() { return this.content.some(...arguments); }
-  contains() { return this.content.contains(...arguments); }
-  invoke() { return this.content.invoke(...arguments); }
-  max() { return this.content.max(...arguments); }
-  min() { return this.content.min(...arguments); }
-  sortBy() { return this.content.sortBy(...arguments); }
-  groupBy() { return this.content.groupBy(...arguments); }
-  shuffle() { return this.content.shuffle(...arguments); }
-  toArray() { return this.content.toArray(...arguments); }
-  size() { return this.content.size(...arguments); }
-  first() { return this.content.first(...arguments); }
-  initial() { return this.content.initial(...arguments); }
-  rest() { return this.content.rest(...arguments); }
-  last() { return this.content.last(...arguments); }
   without() { return this.content.without(...arguments); }
-  indexOf() { return this.content.indexOf(...arguments); }
-  lastIndexOf() { return this.content.lastIndexOf(...arguments); }
-  isEmpty() { return this.content.isEmpty(...arguments); }
-  chain() { return this.content.chain(...arguments); }
-  difference() { return this.content.difference(...arguments); }
-  sample() { return this.content.sample(...arguments); }
-  partition() { return this.content.partition(...arguments); }
-  countBy() { return this.content.countBy(...arguments); }
-  indexBy() { return this.content.indexBy(...arguments); }
 }
 
 export default CollectionProxy;
