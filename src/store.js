1a7bd05efdc3323e76d2a0c76838ce393a8a4879
StanislauTarazevich <s.tarazevich@gmail.com>
2016-11-21 17:23:38 +0300

    create relationships object on model build

--- src/store.js
+++ src/store.js
@@ -91,13 +91,32 @@ class Store {
       attributes = {};
     }
 
-    return this.push({
+    if (!attributes.relationships) {
+      attributes.relationships = {};
+    }
+
+    const entity = {
       data: {
         id: attributes.id,
         type,
-        attributes
+        attributes,
       }
-    });
+    };
+
+    const modelDefinition = this._modelDefinitions[type];
+    if (modelDefinition && typeof modelDefinition.relationships === 'object') {
+      Object.keys(modelDefinition.relationships).forEach((key) => {
+        if (!entity.data.attributes.relationships[key]) {
+          entity.data.attributes.relationships[key] = {
+            data: {
+              type: modelDefinition.relationships[key]
+            }
+          }
+        }
+      });
+    }
+
+    return this.push(entity);
   }
 
   /**
