6d9a97d727ed0e87e059672179fcc25318375848
CircleCi Doc Deploy Backbone.Store <icunningham@grove.co>
2019-05-16 03:07:44 +0000

    [skip ci] Updates

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import HttpAdapter from './http-adapter'
-import JsonApiParser from './json-api-parser'
-import Repository from './repository'
-import Store from './store'
-
-export { HttpAdapter, JsonApiParser, Repository, Store }
