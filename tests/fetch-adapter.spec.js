b42baca59f640cadc77eae1df0856871ce1e59c5
Bill Glesias <williamglesias@MacBook-Pro-8.nc.rr.com>
2019-08-30 16:00:18 -0400

    refactor buildUrl and request methods into http-adapter test spec to be tested by base class

--- tests/fetch-adapter.spec.js
+++ tests/fetch-adapter.spec.js
@@ -96,25 +96,6 @@ describe('HTTP adapter', function () {
     })
   })
 
-  describe('#buildUrl', function () {
-    it('returns the canonical link for a type and id', function () {
-      expect(adapter.buildUrl('foo', 2)).toEqual('/foo/2/')
-      expect(adapter.buildUrl(4, 2)).toEqual('/4/2/')
-      expect(adapter.buildUrl('foo', 0)).toEqual('/foo/0/')
-    })
-
-    it('returns the canonical link for a type', function () {
-      expect(adapter.buildUrl('foo')).toEqual('/foo/')
-      expect(adapter.buildUrl(4)).toEqual('/4/')
-    })
-
-    it('returns the canonical link with a prefix', function () {
-      let adapter = new HttpAdapter({urlPrefix: '/api'})
-      expect(adapter.buildUrl('foo', 2)).toEqual('/api/foo/2/')
-      expect(adapter.buildUrl('foo')).toEqual('/api/foo/')
-    })
-  })
-
   describe('#get', function () {
     it('returns a parsed resource from the network', function () {
       let payload = {data: {id: 123, attributes: {foo: 'asdf'}}}
